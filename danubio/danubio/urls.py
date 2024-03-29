9f90598bf2cda6800739f9385da8cc95867255b0
Jose Viquez <joseviquez@MacBook-Pro-de-Jose.local>
2024-03-29 13:49:48 -0600

    Adding default URL

--- danubio/danubio/urls.py
+++ danubio/danubio/urls.py
@@ -2,8 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/orders/')),
     path('admin/', admin.site.urls),
     path('orders/', include('orders.urls')),
     path('inventory/', include('inventory.urls')),
